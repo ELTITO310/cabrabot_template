62531aa6850e071a96d7c26383dca2d81e9ed753
ELTITO310 <romualdoromualdo181@gmail.com>
2022-02-04 18:57:15 -0500

    Token update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Client, Collection, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { registerCmds, registerEvents } = require('./utils/registerHandler');
@@ -8,4 +9,4 @@ client.cooldowon = new Collection();
 registerCmds('../handler/commands', client);
 registerEvents('../handler/events', client);
 
-client.login('NzgzNDYzNDY5MjM1Njk5NzMy.X8bHNw.huFij0X3F7N7bC7-9b_coZXUc54')
+client.login(process.env.TOKEN);
